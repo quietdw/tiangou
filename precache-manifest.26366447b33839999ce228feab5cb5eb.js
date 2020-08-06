a04d67133fc1a5e78c1139608a89cee33db458df
quietdw <1044569102@qq.com>
2020-08-06 11:52:37 +0800

    deploy

---
+++ precache-manifest.26366447b33839999ce228feab5cb5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321f3930f884defa4c96d0b2f1b6e7fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e72434f8ee4de06617a",
+    "url": "./static/css/main.16fa14cf.chunk.css"
+  },
+  {
+    "revision": "07694c691eec1f726ae9",
+    "url": "./static/js/2.6cf91572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6cf91572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e72434f8ee4de06617a",
+    "url": "./static/js/main.cb78d22d.chunk.js"
+  },
+  {
+    "revision": "3ca99f66e98554b22744",
+    "url": "./static/js/runtime-main.9d206326.js"
+  },
+  {
+    "revision": "57ea11e19b2216124d7610c477cc11a0",
+    "url": "./static/media/lick.57ea11e1.gif"
+  }
+]);
