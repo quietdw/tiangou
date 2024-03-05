6894c8d62857a04f6ef2f78e8c5beb093eecc5f3
姜大伟 <jiangdawei@mysteel.com>
2024-03-05 11:01:43 +0800

    deploy

---
+++ precache-manifest.97f8aca6b0dd5b21de63388055f0c951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448908f546ca7edbd259024da24d07f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a2cb4107ff1800edd70",
+    "url": "./static/css/main.f5c65ec8.chunk.css"
+  },
+  {
+    "revision": "57e2ed5874504824984a",
+    "url": "./static/js/2.583a787a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.583a787a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2cb4107ff1800edd70",
+    "url": "./static/js/main.93cf609a.chunk.js"
+  },
+  {
+    "revision": "3ca99f66e98554b22744",
+    "url": "./static/js/runtime-main.9d206326.js"
+  },
+  {
+    "revision": "57ea11e19b2216124d7610c477cc11a0",
+    "url": "./static/media/lick.57ea11e1.gif"
+  }
+]);
